0f7ef69168e697bd7350ed839062a18fdec2d070
Oana Agache <oana.luciana.agache@gmail.com>
2022-05-29 18:48:29 +0300

    Update GetToken.js

--- src/GetToken.js
+++ src/GetToken.js
@@ -1,5 +1,10 @@
 import React from "react";
+
 const GetToken = () => {
+  console.log("Getting the auth token...");
+
+  const oauthUrl = "https://api.petfinder.com/v2/oauth2/token;";
+
   return <div></div>;
 };
 
