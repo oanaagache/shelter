a7e2a2a5183ff2e61385f28627c1736dbe16b82c
Oana Agache <oana.luciana.agache@gmail.com>
2022-11-24 23:04:56 +0200

    Update Cards.js

--- src/old/Cards.js
+++ src/old/Cards.js
@@ -4,16 +4,19 @@ import "../components/card/Card.css";
 import CardO from "./CardO";
 import { Client } from "@petfinder/petfinder-js";
 
-const Cards = () => {
+const Cards = (props) => {
   const client = new Client({
     apiKey: "yE34mF5y8uaTkcDpopHWiZnWGoYJX5Ufw59vtFDpOJU78uW5ur",
     secret: "HMpwZ6VMhZwUWXYpb9nVmliqdElYYT96mezpupJk",
   });
 
-  const [name, setName] = useState([]);
+  //console.log("Cards : type");
+  //console.log(type);
+
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
-    client.animal.search({ page: 1, limit: 50 }).then((response) => {
+    client.animal.search({ page: 1, limit: 6 }).then((response) => {
       var animalsArray = [];
       //console.log("animalsArray: ");
       //console.log(animalsArray);
@@ -41,7 +44,7 @@ const Cards = () => {
         //console.log(animal);
         animalsArray.push(animal);
       }
-      setName(animalsArray);
+      setItems(animalsArray);
       //console.log("animalsArray in Cards:");
       //console.log(animalsArray);
     });
@@ -49,7 +52,7 @@ const Cards = () => {
 
   return (
     <>
-      {name.map((item, index) => {
+      {items.map((item, index) => {
         return (
           <>
             <CardO key={index} item={item} />
