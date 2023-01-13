2e80679451c2d5e660adea2c1010c70d8490ec89
Oana Agache <oana.luciana.agache@gmail.com>
2023-01-13 23:41:57 +0200

    Update Main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,15 +5,16 @@ import "../main/Main.css";
 const Main = () => {
   return (
     <div className="main">
-      <div className="main-image ">
-        <img
-          src={logo3}
-          alt="Not found"
-          // style={
-          //   ({ height: "100%" }, { maxWidth: "100%" }, { objectFit: "cover" })
-          // }
-        />
-      </div>
+      <div
+        className="main-image "
+        style={{
+          background: `url(${logo3})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          height: "500px",
+        }}
+      ></div>
 
       <div className="main-text">
         <h3 className="main-title">
@@ -24,5 +25,30 @@ const Main = () => {
     </div>
   );
 };
-
 export default Main;
+
+// export default Main;
+// const Main = () => {
+//   return (
+//     <div className="main">
+//       <div className="main-image ">
+//         <img
+//           src={logo3}
+//           alt="Not found"
+//           // style={
+//           //   ({ height: "100%" }, { maxWidth: "100%" }, { objectFit: "cover" })
+//           // }
+//         />
+//       </div>
+
+//       <div className="main-text">
+//         <h3 className="main-title">
+//           {" "}
+//           We aim to find best companions for homeless pets in need
+//         </h3>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Main;
