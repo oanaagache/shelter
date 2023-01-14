e1e847609d9c7ab2730b6c318ad9ffe68422da5e
Oana Agache <oana.luciana.agache@gmail.com>
2023-01-14 13:28:27 +0200

    Update Login

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -26,29 +26,3 @@ const Main = () => {
   );
 };
 export default Main;
-
-// export default Main;
-// const Main = () => {
-//   return (
-//     <div className="main">
-//       <div className="main-image ">
-//         <img
-//           src={logo3}
-//           alt="Not found"
-//           // style={
-//           //   ({ height: "100%" }, { maxWidth: "100%" }, { objectFit: "cover" })
-//           // }
-//         />
-//       </div>
-
-//       <div className="main-text">
-//         <h3 className="main-title">
-//           {" "}
-//           We aim to find best companions for homeless pets in need
-//         </h3>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Main;
